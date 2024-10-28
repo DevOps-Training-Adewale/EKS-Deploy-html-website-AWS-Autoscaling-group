c13f7ae9badd181d4c195945d1d0aeee066cf1b3
bepoadewale <bepoadewale@yahoo.com>
2024-10-28 07:43:31 +0000

    c10

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "eks-deploy-html-website-aws-autoscaling-group"
+    key            = "html-app/terraform.tfstate"
     region         = "us-east-1"
   }
 }
